c1e8ea847f72e2ea5359d679cdd1726ac36720eb
sinmugyeol <tlsanruf123@gmail.com>
2021-04-27 19:47:10 +0900

    numpy basic grammar

---
+++ Tensor_Manipulation_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+
+# numpy
+t = np.array([0., 1., 2., 3., 4., 5., 6.])
+
+# 차원의 수
+print('Rank of t: ', t.ndim)
+
+# 요소의 수
+print('shape of t: ', t.shape)
+
+# 2차원 배열
+t = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12.]])
+
+print('Rank  of t: ', t.ndim)
+print('shape of t: ', t.shape)
+
